407096d086e635f8255a7f8ba56689cc0b4e807a
SergLam <lemur246@mail.ru>
2016-05-26 23:49:06 +0300

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -1,7 +1,7 @@
 #include "mainwindow.h"
 #include "QDesktopServices"
 void MainWindow::Print(){
-    // Экранирование символов
+    // JavaScript code for creating Google Maps page
     QString b = "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">";
     QString b1 = "<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"ru\" lang=\"ru\"><head>";
     QString b2 = "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=windows-1251\"/>";
@@ -13,12 +13,12 @@ void MainWindow::Print(){
     QString b8 = "</style></head><body onload=\"initialize()\"><div id=\"map_canvas\" style=\"width:100%; height:100%\"></div>";
     QString b9 = "<script type=\"text/javascript\" src=\"http://maps.google.com/maps/api/js?sensor=false\"></script><script type=\"text/javascript\">";
     QString b10 = "function initialize() {var latlng = new google.maps.LatLng(49.99, 36.23), myOptions = {zoom: 8, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP};";
-    QString b11 = "var map = new google.maps.Map(document.getElementById(\"map_canvas\"), myOptions); // Координаты для линии";
+    QString b11 = "var map = new google.maps.Map(document.getElementById(\"map_canvas\"), myOptions); // ГЉГ®Г®Г°Г¤ГЁГ­Г ГІГ» Г¤Г«Гї Г«ГЁГ­ГЁГЁ";
     QString b12 = "var polylineCoords = [new google.maps.LatLng(59.95, 30.40), new google.maps.LatLng(59.95, 30.50), new google.maps.LatLng(45.05, 30.50)];";
     QString b13 = "var polyline = new google.maps.Polyline({path: polylineCoords, strokeColor: \"#FF0000\", strokeOpacity: 1.0, strokeWeight: 2 });";
     QString b14 = "polyline.setMap(map);}";
     QString b15 = "</script></body></html>";
-    // Замена координат на вычисленные
+    // Set points coordinates
     QString main_string="var polylineCoords = [";
     for(int i = 0;i<latitude1.count();i++){
         QString buf = "new google.maps.LatLng("+QString::number(latitude1[i])+", "+QString::number(longitude1[i])+"), ";
@@ -27,11 +27,11 @@ void MainWindow::Print(){
         }
         main_string=main_string+buf;
     }
-    // Обратно в массив строк
+    
     b12=main_string;
-    // ЗАПИСЬ В ФАЙЛ!
-    ///////////////////// Сохранение в файл ////////////////////////
-    QString FileSaving = QFileDialog::getSaveFileName(this,"Сохранить маршрут(Укажите путь и имя файла)","D:/","Файл html(*.html)");
+
+    //Saving //
+    QString FileSaving = QFileDialog::getSaveFileName(this,"Save html document for viewing plane trajectory","D:/","File html(*.html)");
     //qDebug()<<FileSaving;
     QFile file(FileSaving);
     if (FileSaving.isEmpty()){
@@ -39,7 +39,7 @@ void MainWindow::Print(){
     }else{
         if(!file.open(QIODevice::WriteOnly|QIODevice::Text)){
             QMessageBox::StandardButton Save;
-            Save=QMessageBox::critical(this,"Ошибка","Ошибка сохранения",QMessageBox::Ok);
+            Save=QMessageBox::critical(this,"ГЋГёГЁГЎГЄГ ","ГЋГёГЁГЎГЄГ  Г±Г®ГµГ°Г Г­ГҐГ­ГЁГї",QMessageBox::Ok);
             return;
         }
     }
